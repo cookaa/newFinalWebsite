6284a64f2e5ac367a8027b4ccb5d8be492d42564
Adam Cook <adam@Adams-MacBook-Pro-15.local>
2019-04-26 15:38:38 -0400

    adding to about pae and page not found 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,9 @@ import Layout from '../components/Layout'
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>404 PAGE NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... such sadness.</p>
+      <p>But keep on looking, theres lots of chocolate to discover!</p>
     </div>
   </Layout>
 )
